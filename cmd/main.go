c5c0d428c440a630729e2ac65cc45cb427cc443c
William Lea <wjlea93@gmail.com>
2024-09-27 11:19:14 +0100

    set up server

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/WilliamJohnathonLea/restaurants-api/server"
+	"github.com/joho/godotenv"
+)
+
 func main() {
-	
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("failed to load environment")
+	}
+
+	dbUsername := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+
+	amqpUsername := os.Getenv("AMQP_USERNAME")
+	amqpPassword := os.Getenv("AMQP_PASSWORD")
+	amqpHost := os.Getenv("AMQP_HOST")
+
+	port, _ := strconv.Atoi(os.Getenv("SERVER_PORT"))
+
+	server := server.New(
+		server.WithPort(port),
+	)
+
+	server.Run()
+
 }
---
+++ server/server.go
@@ -0,0 +1,56 @@
+package server
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gocraft/dbr/v2"
+)
+
+type ServerOpt func(*ServerApp)
+
+type RouteHandler func(*ServerApp) gin.HandlerFunc
+
+type ServerApp struct {
+	Port      int
+	Router    *gin.Engine
+	DbSession *dbr.Session
+}
+
+func New(opts ...ServerOpt) *ServerApp {
+	app := &ServerApp{}
+	router := gin.Default()
+	port := 8080
+
+	app.Router = router
+	app.Port = port
+
+	for _, opt := range opts {
+		opt(app)
+	}
+
+	return app
+}
+
+func WithDbSession(sess *dbr.Session) ServerOpt {
+	return func(sa *ServerApp) {
+		sa.DbSession = sess
+	}
+}
+
+func WithPort(port int) ServerOpt {
+	return func(sa *ServerApp) {
+		sa.Port = port
+	}
+}
+
+func WithRoute(method, path string, handler RouteHandler) ServerOpt {
+	return func(sa *ServerApp) {
+		sa.Router.Handle(method, path, handler(sa))
+	}
+}
+
+func (sa *ServerApp) Run() error {
+	addr := fmt.Sprintf(":%d", sa.Port)
+	return sa.Router.Run(addr)
+}
