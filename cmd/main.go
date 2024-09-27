509ce53aab47b0cb2ed5c7f91a6adb1af9a008b5
William Lea <wjlea93@gmail.com>
2024-09-27 12:28:23 +0100

    add db connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"strconv"
 
 	"github.com/WilliamJohnathonLea/restaurants-api/server"
+	"github.com/gocraft/dbr/v2"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -25,8 +28,26 @@ func main() {
 
 	port, _ := strconv.Atoi(os.Getenv("SERVER_PORT"))
 
+	// Open DB connection
+	dbUrl := fmt.Sprintf(
+		"postgres://%s:%s@%s/restaurants?sslmode=disable",
+		dbUsername,
+		dbPassword,
+		dbHost,
+	)
+	conn, err := dbr.Open("postgres", dbUrl, nil)
+	if err != nil {
+		log.Fatalf("error opening db connection %+v", err)
+	}
+	defer conn.Close()
+
+	sess := conn.NewSession(nil)
+	defer sess.Close()
+
+	// Set up server
 	server := server.New(
 		server.WithPort(port),
+		server.WithDbSession(sess),
 	)
 
 	server.Run()
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gin-gonic/gin v1.10.0
 	github.com/gocraft/dbr/v2 v2.7.6
 	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.4
 )
 
 require (
